163c673e768668a653e128b3b417a54a8801c96a
Mark Harmstone <mark@harmstone.com>
2025-08-13 15:31:23 +0100

    btrfs: add do_remap param to btrfs_discard_extent()
    btrfs_discard_extent() can be called either when an extent is removed
    or from walking the free-space tree. With a remapped block group these
    two things are no longer equivalent: the extent's addresses are
    remapped, while the free-space tree exclusively uses underlying
    addresses.
    
    Add a do_remap parameter to btrfs_discard_extent() and
    btrfs_map_discard(), saying whether or not the address needs to be run
    through the remap tree first.
    
    Signed-off-by: Mark Harmstone <mark@harmstone.com>

--- fs/btrfs/extent-tree.h
+++ fs/btrfs/extent-tree.h
@@ -163,7 +163,7 @@ int btrfs_drop_subtree(struct btrfs_trans_handle *trans,
 			struct extent_buffer *parent);
 void btrfs_error_unpin_extent_range(struct btrfs_fs_info *fs_info, u64 start, u64 end);
 int btrfs_discard_extent(struct btrfs_fs_info *fs_info, u64 bytenr,
-			 u64 num_bytes, u64 *actual_bytes);
+			 u64 num_bytes, u64 *actual_bytes, bool do_remap);
 int btrfs_trim_fs(struct btrfs_fs_info *fs_info, struct fstrim_range *range);
 
 #endif
